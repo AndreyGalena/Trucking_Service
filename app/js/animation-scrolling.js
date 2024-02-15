4b7b9c83d333f2be9dfe53bd354308b73f216abb
AndreyGalena <lilyga12.13@gmail.com>
2024-02-15 21:29:44 +0200

    21:29 15.02.24

--- app/js/animation-scrolling.js
+++ app/js/animation-scrolling.js
@@ -1,21 +1,21 @@
-function onEntry(entry) {
-    console.log("Функция");
-    entry.forEach(change => {
-        if (change.isIntersecting) {
-            change.target.classList.add('element-show');
-            console.log("добавил element-show");
-        }
-    });
-}
+// function onEntry(entry) {
+//     console.log("Функция");
+//     entry.forEach(change => {
+//         if (change.isIntersecting) {
+//             change.target.classList.add('element-show');
+//             console.log("добавил element-show");
+//         }
+//     });
+// }
 
-let options = { threshold: [0.5] };
-let observer = new IntersectionObserver(onEntry, options);
-console.log(observer);
+// let options = { threshold: [0.5] };
+// let observer = new IntersectionObserver(onEntry, options);
+// console.log(observer);
 // К какому элементу нужно добавить element-show
-let elements = document.querySelectorAll('.logo-footer');
-console.log(elements);
+// let elements = document.querySelectorAll('.logo-footer');
+// console.log(elements);
 // К этим элементам .element-animation добовляем объект(element-show)
-for (let elm of elements) {
-    observer.observe(elm);
-    console.log("добавило элемент", elm);
-}
+// for (let elm of elements) {
+//     observer.observe(elm);
+//     console.log("добавило элемент", elm);
+// }
